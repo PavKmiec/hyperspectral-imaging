0f7e2b49a45f902708427c58fe8b031348a7ec09
PavKmiec <pawelkmiec07@gmail.com>
2021-08-07 19:19:36 +0100

    added comments and prints to console, aded links to readme file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hyperspectral-imaging
-Getting familiar with hyperspectral imaging data analysis 
+Getting familiar with hyperspectral imaging data analysis
+
+Writeup/Notes:
+
+Part 1: [Getting familiar with Hyperspectral imaging data analysis.](https://learningstuffblog.home.blog/2021/08/07/getting-familiar-with-hyperspectral-imaging-data-analysis-loading-the-data/)
+
+
+Part 2: [Hyperspectral Cube – Getting familiar with data exploration](https://learningstuffblog.home.blog/2021/08/07/part-2-hyperspectral-cube-getting-familiar-with-data-exploration/)
--- data-analysis/data.py
+++ data-analysis/data.py
@@ -10,9 +10,8 @@ from sklearn.decomposition import PCA
 ##
 
 
-# loading the data
-
 '''
+loading the data
 X - input: 3D
 y - output: 2D
 Returns: data and ground truth
@@ -20,36 +19,63 @@ Returns: data and ground truth
 def load_data():
     X = loadmat('data/PaviaU.mat')['paviaU']
     y = loadmat('data/PaviaU_gt.mat')['paviaU_gt']
-    print("X shape: ", X.shape)
-    print("y shape: ", y.shape)
+    print('X shape: ', X.shape)
+    print('y shape: ', y.shape)
     #print(X[0])
     return X, y
-
 
 X, y = load_data()
 
 
-# gettin the pixels and saving to csv file
+'''
+gettin the pixels, examining shapes, 
+making data frame and saving to csv file
+'''
 def get_pixels(X, y):
     # extract pixels/reshape:
-    # each pixel is a vector of lenght of the number of bands of HSI
-    q = X.reshape(-1, X.shape[2])
+    # each pixel is a vector of length of the number of bands of HSI
 
-    # print(q[0])
+    # examine befor reshape
+    print('X0')
+    print(X[0])
+    print('X0 shape')
+    print(X[0].shape, '\n')
+
+    # reshape
+    q = X.reshape(-1, X.shape[2])
+    
+    # examine after reshape 
+    print('Q0')
+    print(q[0])
+    print('Q0 shape')
+    print(q[0].shape, '\n')
+
+    # X shape
+    print('X-shape ', X.shape)
+    # q shape
+    print('q-shape ', q.shape, '\n')
+
+
+    # plot vector at index q[0]
+    plt.plot(q[0])
+    plt.show()
+
     '''
-    print("q shape: ", q.shape)
+    print('q shape: ', q.shape)
     q shape:  (207400, 103)
     610x340
     '''
+
+
     # data frame
     df = pd.DataFrame(data = q)
 
     # dataframe with classes
-    df = pd.concat([df, pd.DataFrame(data = y.ravel())], axis = 1) 
+    df = pd.concat([df, pd.DataFrame(data = y.ravel())], axis = 1)
 
     # column names
     df.columns = [f'band {i}' for i in range(1, X.shape[2] + 1)] + ['class']
-    #print(df.head())
+    print(df.head())
 
     # save to csv file
     df.to_csv('data_set.csv')
@@ -58,25 +84,23 @@ def get_pixels(X, y):
 
 df = get_pixels(X, y)
 
-#print(df.head())
 
 
 '''
-exploring the data
+Dimensionality reduction using PCA,
+further exploration of the data
+and adding class labels
 ''' 
-###
-# Dimension reduction
-###
-
 
 # reducing to 3D using PCA
-# way to reduce the dimensionality of feature space
+# a way to reduce the dimensionality of feature space
 # refs: 
 # https://online.stat.psu.edu/stat505/book/export/html/670
 # https://www.youtube.com/watch?v=FgakZw6K1QQ
 # https://www.youtube.com/watch?v=HMOI_lkzW08
 # https://towardsdatascience.com/visualising-the-classification-power-of-data-54f5273f640
 
+# PCA
 pca = PCA(n_components=3)
 
 # data.iloc[<row selection>, <column selection>] //remove
@@ -147,18 +171,6 @@ counts = qq['class_labels'].value_counts()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 # img_data = loadmat('data/PaviaU.mat')
 # def display_band():
 #     # check data
@@ -174,15 +186,15 @@ counts = qq['class_labels'].value_counts()
 #     # check shape
 #     print(data_In.shape)
 
-#     # visualise sample image, band at index 0
-#     plt.figure(figsize=(10, 10))
+#     # visualise sample image, band at index 102
+#     plt.figure(figsize=(8, 8))
 #     plt.imshow(data_In[:, :, 102])
 #     plt.show()
 
 
 #     # index = 0
 #     # for i in data_In:
-#     #     plt.figure(figsize=(10, 10))
+#     #     plt.figure(figsize=(8, 8))
 #     #     plt.imshow(data_In[:,:,index], interpolation='nearest')
 #     #     plt.show()
 #     #     index += 1
@@ -190,10 +202,6 @@ counts = qq['class_labels'].value_counts()
 # #print(data_keys)
 
 # display_band()
-
-
-
-
 
 
 
